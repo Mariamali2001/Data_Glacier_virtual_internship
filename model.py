f1186b20ed8e3d1d5050254ad1024975f0bfdcaa
mariam ali <mariambashandy22@gmail.com>
2022-08-27 14:06:55 +0200

    draft

--- model.py
+++ model.py
@@ -2,22 +2,20 @@ import numpy as np
 import pandas as pd
 import pickle 
 
-data = pd.read_csv('toy_dataset.csv')
-
-data = data.replace(['Yes', 'No'], ['0', '1'], 'illness')
-data = data.withColumn("ill",data.illness.cast('int'))
-data.show()
-
-# X=data.iloc[:,:4]
-# Y=data.iloc[:,-1]
-
-# from sklearn.linear_model import LinearRegression
-# regressor =LinearRegression() 
-
-# regressor.fit(X,Y)
-
-# pickle.dump(regressor, open('model.pkl','wb')) 
+data = pd.read_csv('E:/solo projects/Data_Glacier_virtual_internship/Data_Glacier_virtual_internship/week 4/toy_dataset.csv')
 
 
-# model =pickle.load(open('model.pkl','rb')) 
-# print(model.predict([[2, 2200, 5]]))
+
+X=data.iloc[:,:4]
+Y=data.iloc[:,-1]
+
+from sklearn.linear_model import LinearRegression
+regressor =LinearRegression() 
+
+regressor.fit(X,Y)
+
+pickle.dump(regressor, open('model.pkl','wb')) 
+
+
+model =pickle.load(open('model.pkl','rb')) 
+print(model.predict([[2, 2200, 5]]))
