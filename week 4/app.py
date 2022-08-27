b0a22c2fc92d9e2a6b6509b1aaf6fd05573be3bb
mariam ali <mariambashandy22@gmail.com>
2022-08-27 14:32:57 +0200

    ..

--- week 4/app.py
+++ week 4/app.py
@@ -4,20 +4,19 @@ import pickle
 
 
 app=Flask(__name__)
-# model = pickle.load(open('model.pkl','rb'))
+model = pickle.load(open('model.pkl','rb'))
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def home():
     return render_template('index.html')
 
 
-# @app.route('/predict',methods=['POST'])
-# def predict():
-#     int_features = [int(x) for x in request.form.values()]
-#     final_features = [np.array(int_features)] 
-#     prediction = model.predict(final_features) 
-#     output =round(prediction[0], 2) 
-#     return render_template('index.html', prediction_text='illness should be $ {}'.format(output))
+@app.route('/predict',methods=['POST'])
+def predict():
+    features = [float(x) for x in request.form.values()]
+    final_features = [np.array(features)] 
+    prediction = model.predict(final_features) 
+    return render_template('index.html', prediction_text='The flower species is$ {}'.format(prediction))
 
 if __name__ == '__main__':
-     app.run(port=3000,debug=True)
+     app.run(debug=True)
