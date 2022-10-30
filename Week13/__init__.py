89dbf8265ab920930305250edc95ed6b95c1ae8e
mariam ali <mariambashandy22@gmail.com>
2022-10-30 12:09:22 +0200

    Deployment Flask done + Final Report

--- Week13/__init__.py
+++ Week13/__init__.py
@@ -3,8 +3,8 @@ from flask import Flask , request,render_template
 import numpy as np
 import pickle
 
-app=Flask(__name__)
-model = pickle.load(open('Model/model_gbm.pk','rb'))
+app=Flask(__name__, template_folder='E:/solo projects/Data_Glacier_virtual_internship/Data_Glacier_virtual_internship/Week13/templates/index.html')
+model = pickle.load(open('E:/solo projects/Data_Glacier_virtual_internship/Data_Glacier_virtual_internship/Week13/Model/model_gbm.pk','rb'))
 
 @app.route('/')
 def home():
@@ -22,6 +22,6 @@ def predict():
     
     return render_template('index.html', prediction_text='The Presistent_Flag is {}'.format(prediction))
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # app.run(port =5000,debug=True)
     app.run(host='127.0.0.1', port =5000,debug=True, use_reloader=False)
