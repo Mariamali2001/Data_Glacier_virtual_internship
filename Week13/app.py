f3217dc2f5ab4e698e99e8496e66a2201d0b8190
mariam ali <mariambashandy22@gmail.com>
2022-10-30 11:51:52 +0200

    updates

---
+++ Week13/app.py
@@ -0,0 +1,27 @@
+from unittest import result
+from flask import Flask , request,render_template
+import numpy as np
+import pickle
+
+app=Flask(__name__)
+model = pickle.load(open('Model/model_gbm.pk','rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    
+
+
+    features = [float(x) for x in request.form.values()]
+    final_features = [np.array(features)] 
+    prediction = model.predict(final_features)
+    
+    return render_template('index.html', prediction_text='The Presistent_Flag is {}'.format(prediction))
+
+if __name__ == '__main__':
+    # app.run(port =5000,debug=True)
+    app.run(host='127.0.0.1', port =5000,debug=True, use_reloader=False)
